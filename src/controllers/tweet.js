c0ac23182147aa773e579e156b6a1bf40d968b93
Alex Sandro A. Souza <asasouza.alex@gmail.com>
2019-06-09 00:29:58 -0300

    Tweet Update

--- src/controllers/tweet.js
+++ src/controllers/tweet.js
@@ -3,44 +3,32 @@ const { validationResult } = require('express-validator/check');
 // imports
 const Tweet = require('../models/tweet');
 
-exports.list = async (req, res, next) => {
-	let { query: { limit, offset } } = req;
-	const { user } = req;
-	try {
-		limit = parseInt(limit, 10) || 20;
-		offset = parseInt(offset, 10) || 0;
-		const tweets = await Tweet.find({
-			$or: [
-				{ author: user },
-				{ author: { $in: user.following } }
-
-			]
-		})
-		.populate('author', ['username', 'picture'])
-		.skip(offset)
-		.limit(limit)
-		.sort({ createdAt: -1 });
-
-		res.json({ message: 'Tweets Founded', tweets });
-	} catch (err) {
-		if (!err.statusCode) {
-			err.statusCode = 500;
-		}
-		return next(err);
+exports.create = async (req, res, next) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		const error = new Error('Validation Failed');
+		error.statusCode = 422;
+		error.data = errors.array();
+		return next(error);
 	}
-};
 
-exports.details = async (req, res, next) => {
-	const { params: { id } } = req;
 	try {
-		const tweet = await Tweet.findById(id).populate('author', ['username', 'picture']);
+		const { user, body: { content } } = req;
+		const tweet = await new Tweet({
+			author: user,
+			content,
+		}).save();
+
 		if (!tweet) {
-			const error = new Error('Tweet not found!');
-			error.statusCode = 404;
-			throw error;
+			throw new Error();
 		}
 
-		res.json({ message: 'Tweet found', tweet });
+		tweet.author = { username: user.username, picture: user.picture, _id: user._id };
+
+		res.status(201).json({ 
+			message: 'Tweet created!', 
+			tweet
+		});
 	} catch (err) {
 		if (!err.statusCode) {
 			err.statusCode = 500;
@@ -73,7 +61,54 @@ exports.delete = async (req, res, next) => {
 	}
 };
 
-exports.create = async (req, res, next) => {
+exports.details = async (req, res, next) => {
+	const { params: { id } } = req;
+	try {
+		const tweet = await Tweet.findById(id).populate('author', ['username', 'picture']);
+		if (!tweet) {
+			const error = new Error('Tweet not found!');
+			error.statusCode = 404;
+			throw error;
+		}
+
+		res.json({ message: 'Tweet found', tweet });
+	} catch (err) {
+		if (!err.statusCode) {
+			err.statusCode = 500;
+		}
+		return next(err);
+	}
+};
+
+exports.list = async (req, res, next) => {
+	let { query: { limit, offset } } = req;
+	const { user } = req;
+	try {
+		limit = parseInt(limit, 10) || 20;
+		offset = parseInt(offset, 10) || 0;
+		const tweets = await Tweet.find({
+			$or: [
+				{ author: user },
+				{ author: { $in: user.following } }
+
+			]
+		})
+		.populate('author', ['username', 'picture'])
+		.skip(offset)
+		.limit(limit)
+		.sort({ createdAt: -1 });
+
+		res.json({ message: 'Tweets Founded', tweets });
+	} catch (err) {
+		if (!err.statusCode) {
+			err.statusCode = 500;
+		}
+		return next(err);
+	}
+};
+
+exports.update = async (req, res, next) => {
+	const { body: { content }, params: { id }, user } = req;
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
 		const error = new Error('Validation Failed');
@@ -83,22 +118,21 @@ exports.create = async (req, res, next) => {
 	}
 
 	try {
-		const { user, body: { content } } = req;
-		const tweet = await new Tweet({
-			author: user,
-			content,
-		}).save();
-
+		const tweet = await Tweet.findById(id).populate('author', ['username', 'picture']);
 		if (!tweet) {
-			throw new Error();
+			const error = new Error('Tweet not found');
+			error.statusCode = 404;
+			throw error;
+		}
+		if (tweet.author._id.toString() !== user._id.toString()) {
+			const error = new Error('Only the author can update this tweet');
+			error.statusCode = 403;
+			throw error;
 		}
 
-		tweet.author = { username: user.username, picture: user.picture, _id: user._id };
-
-		res.status(201).json({ 
-			message: 'Tweet created!', 
-			tweet
-		});
+		tweet.content = content;
+		await tweet.save();
+		res.json({ message: 'Tweet updated successfully', tweet });
 	} catch (err) {
 		if (!err.statusCode) {
 			err.statusCode = 500;
--- src/routes/tweet.js
+++ src/routes/tweet.js
@@ -13,6 +13,16 @@ routes.get('/tweets/:id', tweetController.details);
 
 routes.delete('/tweets/:id', isAuth, tweetController.delete);
 
+routes.put('/tweets/:id', isAuth, [
+	body('content')
+		.trim()
+		.isLength({ max: 140 })
+		.withMessage('Tweet content max characters is 140')
+		.not()
+		.isEmpty()
+		.withMessage('Tweet content must not be empty')
+], tweetController.update);
+
 routes.post('/tweets', isAuth, [
 	body('content')
 		.trim()
