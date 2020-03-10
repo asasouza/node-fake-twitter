b1f208a6083e39419a0cf33fb1726ea010bf9c4f
Alex <asasouza.alex@gmail.com>
2020-03-10 20:54:45 -0300

    Mudancas para deploy

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-module.exports.MONGODB_URL = process.env.MONGODB_URL || 'mongodb+srv://commonUser:root@faketwitter-sk4bg.mongodb.net/fake-twitter?retryWrites=true';
+module.exports.MONGODB_URL = process.env.MONGODB_URL || 'mongodb+srv://testUser:root@faketwitter-sk4bg.mongodb.net/fake-twitter?retryWrites=true';
 module.exports.JWT_SECRET = process.env.JWT_SECRET || 'jwt_hash_secret';
 
 module.exports.SITE_URL = process.env.SITE_URL || 'http://localhost:3000';
